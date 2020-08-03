063a093c9b2df094f22187a8c54bbe6dfe72d437
19046402 <19046402@LAPTOP-H58J3MT5>
2020-08-03 13:32:57 +0800

    Edited retrieveAllChromebook

--- ResourceCentre_P11/src/ResourceCentre.java
+++ ResourceCentre_P11/src/ResourceCentre.java
@@ -148,11 +148,16 @@ public class ResourceCentre {
 		// write your code here
 		for (int i = 0; i < chromebookList.size(); i++) {
 
-			output += String.format("%-10s %-30s %-10s %-10s %-20s\n", chromebookList.get(i).getAssetTag(),
-					chromebookList.get(i).getDescription(), 
-					ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
-					chromebookList.get(i).getDueDate(),chromebookList.get(i).getOs());
+			output = toString(chromebookList, output, i);
 		}
+		return output;
+	}
+
+	private static String toString(ArrayList<Chromebook> chromebookList, String output, int i) {
+		output += String.format("%-10s %-30s %-10s %-10s %-20s\n", chromebookList.get(i).getAssetTag(),
+				chromebookList.get(i).getDescription(), 
+				ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
+				chromebookList.get(i).getDueDate(),chromebookList.get(i).getOs());
 		return output;
 	}
 	public static void viewAllChromebook(ArrayList<Chromebook> chromebookList) {
